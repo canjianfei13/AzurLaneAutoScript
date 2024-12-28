fefd3b64e370661b8076083d5d1ac6286d3c4780
zhenghan123 <61129862+zhenghan123@users.noreply.github.com>
2024-12-28 22:03:03 +0800

    Update alas.py

--- alas.py
+++ alas.py
@@ -13,7 +13,6 @@ from module.config.utils import deep_get, deep_set
 from module.exception import *
 from module.logger import logger
 from module.notify import handle_notify
-from module.gg_handler.gg_handler import GGHandler
 
 
 class AzurLaneAutoScript:
@@ -548,31 +547,31 @@ class AzurLaneAutoScript:
         AzurLaneConfig.is_hoarding_task = False
         return task.command
 
-    def gg_check(self):
-        if deep_get(self.config.data, "GameManager.GGHandler.Enabled"):
-            logger.info("GG is enabled, check gg package name")
-            if deep_get(self.config.data, "GameManager.GGHandler.GGPackageName") in self.device.list_package():
-                logger.info("GG package name exists")
-            else:
-                logger.critical("GG package name doesn't exist, please check your gg setting")
-                logger.critical("友情翻译：你他妈的GG包名填错了，滚去重填！！！")
-                exit(1)
+    # def gg_check(self):
+    #     if deep_get(self.config.data, "GameManager.GGHandler.Enabled"):
+    #         logger.info("GG is enabled, check gg package name")
+    #         if deep_get(self.config.data, "GameManager.GGHandler.GGPackageName") in self.device.list_package():
+    #             logger.info("GG package name exists")
+    #         else:
+    #             logger.critical("GG package name doesn't exist, please check your gg setting")
+    #             logger.critical("友情翻译：你他妈的GG包名填错了，滚去重填！！！")
+    #             exit(1)
 
-    def loop(self):
-        self.gg_check()
-        logger.set_file_logger(self.config_name)
-        logger.info(f'Start scheduler loop: {self.config_name}')
-        # Try forced task_call restart to reset GG status
-        self.checker.wait_until_available()
-        GGHandler(config=self.config, device=self.device).handle_restart_before_tasks()
-        check_fail = 0
-        while 1:
-            # Check update event from GUI
-            if self.stop_event is not None:
-                if self.stop_event.is_set():
-                    logger.info("Update event detected")
-                    logger.info(f"Alas [{self.config_name}] exited.")
-                    break
+    # def loop(self):
+    #     self.gg_check()
+    #     logger.set_file_logger(self.config_name)
+    #     logger.info(f'Start scheduler loop: {self.config_name}')
+    #     # Try forced task_call restart to reset GG status
+    #     self.checker.wait_until_available()
+    #     GGHandler(config=self.config, device=self.device).handle_restart_before_tasks()
+    #     check_fail = 0
+    #     while 1:
+    #         # Check update event from GUI
+    #         if self.stop_event is not None:
+    #             if self.stop_event.is_set():
+    #                 logger.info("Update event detected")
+    #                 logger.info(f"Alas [{self.config_name}] exited.")
+    #                 break
             # Check game server maintenance
             self.checker.wait_until_available()
             if self.checker.is_recovered():
@@ -600,19 +599,19 @@ class AzurLaneAutoScript:
                 del_cached_property(self, 'config')
                 continue
 
-            # Check GG config before a task begins (to reset temporary config), and decide to enable it.
-            GGHandler(config=self.config, device=self.device).check_config()
-            try:
-                GGHandler(config=self.config, device=self.device).check_then_set_gg_status(inflection.underscore(task))
-                check_fail = 0
-            except GameStuckError:
-                del_cached_property(self, 'config')
-                check_fail += 1
-                if check_fail <= 3:
-                    continue
-                else:
-                    logger.critical('Maybe your emulator died, trying to restart it')
-                    self.device.emulator_start()
+            # # Check GG config before a task begins (to reset temporary config), and decide to enable it.
+            # GGHandler(config=self.config, device=self.device).check_config()
+            # try:
+            #     GGHandler(config=self.config, device=self.device).check_then_set_gg_status(inflection.underscore(task))
+            #     check_fail = 0
+            # except GameStuckError:
+            #     del_cached_property(self, 'config')
+            #     check_fail += 1
+            #     if check_fail <= 3:
+            #         continue
+            #     else:
+            #         logger.critical('Maybe your emulator died, trying to restart it')
+            #         self.device.emulator_start()
 
             # Run
             logger.info(f'Scheduler: Start task `{task}`')
